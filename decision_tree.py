e0618e525df51485eca8ba9e5797205bd4890821
C-AJ <89857745+C-AJ@users.noreply.github.com>
2023-02-11 23:51:34 -0800

    asdf
    YEAH

--- decision_tree.py
+++ decision_tree.py
@@ -16,6 +16,7 @@ import csv
 db = []
 X = []
 Y = []
+x_holder = []
 
 #reading the data in a csv file
 with open('contact_lens.csv', 'r') as csvfile:
@@ -23,16 +24,41 @@ with open('contact_lens.csv', 'r') as csvfile:
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
          db.append (row)
+         if row[0] == 'Young':
+             x_holder.append(1)
+         elif row[0] == 'Presbyopic':
+             x_holder.append(2)
+         else:
+             x_holder.append(3)
+         if row[1] == 'Myope':
+            x_holder.append(1)
+         else:
+            x_holder.append(2)
+         if row[2] == 'No':
+            x_holder.append(1)
+         else:
+            x_holder.append(2)
+         if row[3] == 'Reduced':
+            x_holder.append(1)
+         else:
+            x_holder.append(2)
+         if row[4] == 'No':
+            Y.append(1)
+         else:
+            Y.append(2)
          print(row)
+         X.append(x_holder)
+         x_holder = []
 
 #transform the original categorical training features into numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
 # so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
 #--> add your Python code here
-X = [[1, 1, 1, 1], [2, 1, 1, 2], [3, 1, 1, 1], [3, 1, 1, 2], [2, 1, 2, 2], [1, 1, 2, 2], [1, 2, 1, 1], [3, 1, 2, 1], [2, 2, 1, 1], [1, 1, 2, 1]]
+#X = [[1, 1, 1, 1], [2, 1, 1, 2], [3, 1, 1, 1], [3, 1, 1, 2], [2, 1, 2, 2], [1, 1, 2, 2], [1, 2, 1, 1], [3, 1, 2, 1], [2, 2, 1, 1], [1, 1, 2, 1]]
+print(X)
 
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
 #--> addd your Python code here
-Y = [1, 1, 1, 2, 2, 2, 1, 1, 1, 2]
+#Y = [1, 1, 1, 2, 2, 2, 1, 1, 1, 2]
 
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
